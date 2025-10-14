e6a2e854b2e7390f89cf295cac14504376d8e4be
Asya Lyanova <64890333+pipipyau@users.noreply.github.com>
2025-10-14 12:06:12 +0300

    add script

---
+++ create_folders.py
@@ -0,0 +1,14 @@
+import os
+
+
+try:
+    os.makedirs(os.path.join('data', "input", "ligand"), exist_ok=True)
+    os.makedirs(os.path.join('data', "input", "receptor"), exist_ok=True)
+    os.makedirs(os.path.join('data', "output", "alphafold3", "ligand"), exist_ok=True)
+    os.makedirs(os.path.join('data', "output", "alphafold3", "receptor"), exist_ok=True)
+    os.makedirs(os.path.join('data', "output", "megadock"), exist_ok=True)
+    os.makedirs(os.path.join('data', "output", "prodigy"), exist_ok=True)
+    print("Done.")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
